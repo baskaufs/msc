0d0813d0f12a20627b71c1c0dd15cef133f80bc4
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2018-05-04 12:32:50 -0500

    update image saver

--- python/save-image.py
+++ python/save-image.py
@@ -1,15 +1,33 @@
 import requests
 import shutil
+import csv #library to read/write/parse CSV files
 
-# Write data to file
 url = "https://farm9.staticflickr.com/8154/7660206660_92d05c90e3_o.jpg"
-filename = "7660206660_92d05c90e3_o.jpg"
-response = requests.get(url)
-status = response.status_code
-print(status)
-with open (filename,'wb') as out_file:
-    for chunk in response.iter_content(chunk_size=128):
-        out_file.write(chunk)
+path = "c:\\test\\chapman-output\\"
+infile = "c:\\test\\flickr\\urls.txt"
+
+
+
+# Open the CSV file that will store the HTTP response codes for each Get 
+statusFile = open(path + "status.csv", 'w', newline='')
+statusOutput = csv.writer(statusFile)
+
+# open the text file containing the URLs for the highres images
+topHatCsv = open(infile, newline='')
+topHatData = csv.reader(topHatCsv)
+for gradeRow in topHatData:
+	url = gradeRow[0].strip()
+	filename = url.split("/")[4]
+	
+	# Write data to file
+	response = requests.get(url)
+	status = response.status_code
+	outputLine = (status,url)
+	statusOutput.writerow(outputLine)
+	
+	with open (path + filename,'wb') as out_file:
+		for chunk in response.iter_content(chunk_size=128):
+			out_file.write(chunk)
 
 
 
