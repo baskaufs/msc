0a9da3c9d9b4b0247e75320410613ff166c12aef
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2018-05-04 06:18:27 -0500

    update save-image

--- python/save-image.py
+++ python/save-image.py
@@ -4,11 +4,13 @@ import shutil
 # Write data to file
 url = "https://farm9.staticflickr.com/8154/7660206660_92d05c90e3_o.jpg"
 filename = "7660206660_92d05c90e3_o.jpg"
-response = requests.get(url, stream=True)
-#status = requests.status_code
+response = requests.get(url)
+status = response.status_code
+print(status)
 with open (filename,'wb') as out_file:
-	shutil.copyfileobj(response.raw, out_file)
-del response
+    for chunk in response.iter_content(chunk_size=128):
+        out_file.write(chunk)
+
 
 
 
