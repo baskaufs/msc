0e111808673494a9790504e6b4d6311684427a9c
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2018-10-11 16:26:05 -0500

    files for testing rs.tdwg.org server

--- python/rs_tdwg_org/url-test.py
+++ python/rs_tdwg_org/url-test.py
@@ -2,7 +2,7 @@ import requests
 import csv #library to read/write/parse CSV files
 
 baseUrl = 'http://vuswwg-private.jelastic.servint.net/gom'
-header = 'text/turtle'
+header = 'text/html'
 csvObject = open('rs-not-404s.csv', newline='')
 csvData = csv.reader(csvObject)
 outObject = open('output.txt','wt')
@@ -17,7 +17,7 @@ for row in csvData:
         if r.status_code == 404:
             response = ""
         else:
-            response = r.text[:30]
+            response = r.text[:20]
         print(str(r.status_code)+"\t"+row[0]+"\t"+row[1]+"\t"+response)
         print(str(r.status_code)+"\t"+row[0]+"\t"+row[1]+"\t"+response, file=outObject)
     rowNum =+ 1
